c77037039d202cbe84539585b0e60d67ed667052
Gerald E. Butler <gerald.edward.butler@gmail.com>
2021-01-03 16:52:36 -0500

    reformat code with rust-fmt (using format command from VSCode)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
     let name = "John A";
     let min_width = 20;
-    let max_width =30;
-    println!("Hello, {:^MinWidth$.MaxWidth$}!", name, MinWidth=min_width, MaxWidth=max_width);
+    let max_width = 30;
+    println!(
+        "Hello, {:^MinWidth$.MaxWidth$}!",
+        name,
+        MinWidth = min_width,
+        MaxWidth = max_width
+    );
 }
