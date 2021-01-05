87e4a51cfdd85fa74268cc64ba052666aa2a1c57
Gerald E. Butler <gerald.edward.butler@gmail.com>
2021-01-05 12:38:56 -0500

    Remove example code & make it get the command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,5 @@
-fn main() {
-    let name = "John A";
-    let min_width = 20;
-    let max_width = 30;
+use std::env;
 
-    for idx in 1..=1000 {
-        println!(
-            "{Repetition:.<10} Hello, {Name:^MinWidth$.MaxWidth$}! ",
-            Repetition = idx,
-            Name = name,
-            MinWidth = min_width,
-            MaxWidth = max_width
-        );
-    }
+fn main() {
+    let args: Vec<String> = env::args().collect();
 }
