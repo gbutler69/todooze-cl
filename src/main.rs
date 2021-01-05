c9a9f9767e5210bd247e0a53e045bdda4c020ab3
Gerald E. Butler <gerald.edward.butler@gmail.com>
2021-01-05 12:36:39 -0500

    Made loop print the repetion number and made output 1 per line

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@ fn main() {
     let name = "John A";
     let min_width = 20;
     let max_width = 30;
-    println!(
-        "Hello, {:^MinWidth$.MaxWidth$}!",
-        name,
-        MinWidth = min_width,
-        MaxWidth = max_width
-    );
+
+    for idx in 1..=1000 {
+        println!(
+            "{Repetition:.<10} Hello, {Name:^MinWidth$.MaxWidth$}! ",
+            Repetition = idx,
+            Name = name,
+            MinWidth = min_width,
+            MaxWidth = max_width
+        );
+    }
 }
